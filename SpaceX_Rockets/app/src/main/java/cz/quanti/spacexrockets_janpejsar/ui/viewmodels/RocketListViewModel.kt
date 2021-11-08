76ed406f58adc5ac2d2164699004fb011d9585d5
Jan Pejsar <jan.pejsar@quanti.cz>
2021-11-08 19:46:07 +0100

    Created app

--- SpaceX_Rockets/app/src/main/java/cz/quanti/spacexrockets_janpejsar/ui/viewmodels/RocketListViewModel.kt
+++
@@ -1,8 +0,0 @@
-package cz.quanti.spacexrockets_janpejsar.ui.viewmodels
-
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-
-class RocketListViewModel(application: Application): AndroidViewModel(application) {
-
-}
