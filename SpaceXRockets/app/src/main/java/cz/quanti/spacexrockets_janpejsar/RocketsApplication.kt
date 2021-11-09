81ba3728b652f62d1a2b8d8c9eec1ba8c7e1ce02
Jan Pejsar <jan.pejsar@quanti.cz>
2021-11-09 10:05:14 +0100

    Add Hilt for dependency injection

---
+++ SpaceXRockets/app/src/main/java/cz/quanti/spacexrockets_janpejsar/RocketsApplication.kt
@@ -0,0 +1,8 @@
+package cz.quanti.spacexrockets_janpejsar
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RocketsApplication: Application() {
+}
