4583c8eb3aca3c28e7a05e2dae49bf8f5b4cb219
Jan Pejsar <jan.pejsar@quanti.cz>
2021-11-09 08:51:30 +0100

    Delete equals() and hashCode() from all data classes

--- SpaceXRockets/app/src/main/java/cz/quanti/spacexrockets_janpejsar/entities/Mass.kt
+++ SpaceXRockets/app/src/main/java/cz/quanti/spacexrockets_janpejsar/entities/Mass.kt
@@ -3,22 +3,4 @@ package cz.quanti.spacexrockets_janpejsar.entities
 data class Mass(
     val kg: Int,
     val lb: Int
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Mass
-
-        if (kg != other.kg) return false
-        if (lb != other.lb) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = kg
-        result = 31 * result + lb
-        return result
-    }
-}
+)
--- SpaceXRockets/app/src/main/java/cz/quanti/spacexrockets_janpejsar/entities/Rocket.kt
+++ SpaceXRockets/app/src/main/java/cz/quanti/spacexrockets_janpejsar/entities/Rocket.kt
@@ -21,34 +21,4 @@ data class Rocket(
     val secondStage: Stage,
     @field:Json(name="first_flight")
     val firstFlight: String
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Rocket
-
-        if (id != other.id) return false
-        if (name != other.name) return false
-        if (description != other.description) return false
-        if (height != other.height) return false
-        if (diameter != other.diameter) return false
-        if (mass != other.mass) return false
-        if (firstStage != other.firstStage) return false
-        if (secondStage != other.secondStage) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + name.hashCode()
-        result = 31 * result + description.hashCode()
-        result = 31 * result + height.hashCode()
-        result = 31 * result + diameter.hashCode()
-        result = 31 * result + mass.hashCode()
-        result = 31 * result + firstStage.hashCode()
-        result = 31 * result + secondStage.hashCode()
-        return result
-    }
-}
+)
