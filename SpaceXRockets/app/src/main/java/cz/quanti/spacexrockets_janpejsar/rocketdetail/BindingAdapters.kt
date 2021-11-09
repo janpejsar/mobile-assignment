a452ba0bbc1d206013a0e20f864081cd76402aa0
Jan Pejsar <jan.pejsar@quanti.cz>
2021-11-09 16:55:06 +0100

    Add Overview, Parameters and First Stage chapters to RocketDetailFragment

---
+++ SpaceXRockets/app/src/main/java/cz/quanti/spacexrockets_janpejsar/rocketdetail/BindingAdapters.kt
@@ -0,0 +1,48 @@
+package cz.quanti.spacexrockets_janpejsar.rocketdetail
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import cz.quanti.spacexrockets_janpejsar.R
+import cz.quanti.spacexrockets_janpejsar.spacexdatabase.entities.DimensionDatabaseEntity
+import cz.quanti.spacexrockets_janpejsar.spacexdatabase.entities.MassDatabaseEntity
+import kotlin.math.roundToInt
+
+object BindingAdapters {
+    @BindingAdapter("dimension")
+    @JvmStatic
+    fun setDimension(view: TextView, dimension: DimensionDatabaseEntity?) {
+        view.text = if (dimension == null) {
+            null
+        } else {
+            view.context.resources.getString(
+                R.string.dimension_meters,
+                dimension.meters.roundToInt()
+            )
+        }
+    }
+
+    @BindingAdapter("mass")
+    @JvmStatic
+    fun setMass(view: TextView, mass: MassDatabaseEntity?) {
+        view.text = if (mass == null) {
+            null
+        } else {
+            view.context.resources.getString(
+                R.string.mass_tons,
+                mass.kg / 1000
+            )
+        }
+    }
+
+    @BindingAdapter("reusable")
+    @JvmStatic
+    fun reusable(view: TextView, reusable: Boolean?) {
+        view.setText(
+            if (reusable != null && reusable) {
+                R.string.reusable
+            } else {
+                R.string.not_reusable
+            }
+        )
+    }
+}
